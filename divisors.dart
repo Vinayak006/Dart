cf36d659ba8684b44683f064505d614404de4591
Vinayak006 <vinayakamirtharaj28@gmail.com>
2022-09-09 13:17:51 +0530

    done

--- divisors.dart
+++ divisors.dart
@@ -2,12 +2,14 @@ import 'dart:io';
 void main(){
   print("Enter a number:");
   String? number = stdin.readLineSync();
+  print(divisorsOf(int.parse(number!)));
+}
+
+String divisorsOf(int n){
+  if(n<1) return "enter positive number..";
   List divisors = [];
-  for(int i =1; i<=int.parse(number!); i++){
-    if(int.parse(number)%i==0) divisors.add(i);
+  for (int i = 1; i <= n; i++) {
+    if (n % i == 0) divisors.add(i);
   }
-  print(
-    (divisors.isNotEmpty) 
-      ? "Divisors of a number $number are \n ${divisors.join(", ")}"
-      : "Negative numbers are not valid..");
+  return "Divisors of a number $n are \n ${divisors.join(", ")}";
 }
